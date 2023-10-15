2f62086818dc70d4babb4e0cf681e25cf5f62f0d
joaogabriel87 <joagabriel9@gmail.com>
2023-10-15 14:45:32 -0300

    Adição do swagger na api

--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
 using Chapter_API.Contexts;
 using Chapter_API.Repositories;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +17,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+//Configura o Swagger
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+       Title= "ChapterApi",
+       Version = "v1" 
+    });
+});
+
 var app = builder.Build();
+
+app.UseDeveloperExceptionPage();
+//ativa o middleware para uso do swagger
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+c.SwaggerEndpoint("/swagger/v1/swagger.json", "ChapterApi v1"));
 
 app.UseRouting();
 
